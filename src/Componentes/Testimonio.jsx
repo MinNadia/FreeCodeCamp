122791901169c5aecd07d5bab2f260b13af6518f
MinNadia <103218110+MinNadia@users.noreply.github.com>
2022-08-24 01:38:15 -0600

    FreeCode

---
+++ src/Componentes/Testimonio.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../Hojas-de-estilo/Testimonio.css';
+
+
+function Testimonio(props) {
+    return (
+      <div className='contenedor-testimonio'>
+            <img
+                className='imagen-testimonio'
+                src={require(`../Imagenes/testimonio-${props.imagen}.png`)}
+                alt= 'Foto de {props.nombre}' />
+            <div className='contenedor-texto-testimonio'>
+                 <p className='nombre-testimonio'>
+                  <strong>{props.nombre}</strong> en {props.pais}</p>
+                 <p className='cargo-testimonio'>
+                  {props.cargo} en <strong>{props.empresa}</strong></p>
+                 <p className='texto-testimonio'>"{props.testimonio}"</p>
+            </div>
+      </div>
+    );
+}
+
+export default Testimonio;
